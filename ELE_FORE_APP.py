c27cb1cafe7d7d95a73b80d62aaba8556db8f0cf
kingdrawon <kingdrawon@gmail.com>
2020-11-11 15:13:15 +0800

    add请求结果状态

--- ELE_FORE_APP.py
+++ ELE_FORE_APP.py
@@ -3,12 +3,11 @@
 # Author:Drawon
 # Time:2020/11/6 17:08
 # Version:python 3.7.6
-
-
+import json
 import logging
-from flask import Flask, request, abort, make_response
+from flask import Flask, request, make_response
 from flasgger import Swagger,swag_from
-from EleForeApp.ele_forecast import valueForecast
+from ELE_FORE import valueForecast
 
 
 app = Flask(__name__)
@@ -29,20 +28,27 @@ swagger_config = {
     "specs_route": "/apidocs/"
 }
 Swagger(app,config=swagger_config)
-@app.route('/api/valueFore/', methods=['POST','GET'])
+@app.route('/api/valueFore/', methods=['POST'])
 @swag_from('valueFore.yml')
 def forecastValue():
     try:
         if "file" not in request.files:
             return "No FILE"
         filebytes = request.files["file"]
-        print('执行前',filebytes)
-        value =  valueForecast(filebytes).to_dict()
+        value =  valueForecast(filebytes).to_json()
     except Exception as e:
         logging.exception('出现错误',exc_info=e)
-        return make_response('异常')
+        return status('',str(e),500)
     else:
-        return value
+        return status(value)
+
+
+def status(data, exc='', status=200):
+    try:
+        return json.dumps({"code": status, "data": data, "message": exc}, ensure_ascii=False)
+    except Exception :
+        return json.dumps({"code": 500, "data": {}, "message": exc}, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1",port=5100,debug=True)
